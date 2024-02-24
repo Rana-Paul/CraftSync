ac7d01c0eda27bcaefbf44cd4d5a6ff2eaca6015
Rana Paul <ranapaul741223@gmail.com>
2024-02-24 18:55:35 +0530

    change redirect url

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -68,7 +68,10 @@ export const authOptions: NextAuthOptions = {
       console.log("url", url);
       console.log("baseUrl", baseUrl);
 
-      return "/dashboard";
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
     },
   },
 };
