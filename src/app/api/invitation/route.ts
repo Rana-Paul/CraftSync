1c46fbdff9523f185682bd5b78e5be916059aca5
Rana Paul <ranapaul741223@gmail.com>
2024-03-01 19:30:36 +0530

    error logic in api

--- src/app/api/invitation/route.ts
+++ src/app/api/invitation/route.ts
@@ -5,9 +5,29 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
-  const { workspaceId, code, name } = await request.json();
+  const { workspaceId, code } = await request.json();
   try {
-    
+    const invitation =  await db.invitation.findFirst({
+        where: {
+            AND: [
+                {invitation_code: code},
+                {workspaceId: workspaceId}
+            ]
+        }
+    });
+
+    if (!invitation) {
+        return NextResponse.json({
+            message: "Invitation not found",
+            status: 404
+        })
+    }
+    if(invitation.invitation_code !== code){
+        return NextResponse.json({
+            message: "Invalid invitation code",
+            status: 401
+        })
+    }
   } catch (error) {
     throw new Error("Something went wrong");
   }
