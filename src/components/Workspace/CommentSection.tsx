55e5cfffd03d968b85d03fd2398d43e53a1334c0
Rana Paul <ranapaul741223@gmail.com>
2024-03-06 18:59:21 +0530

    install input box

--- src/components/Workspace/CommentSection.tsx
+++ src/components/Workspace/CommentSection.tsx
@@ -6,19 +6,14 @@ interface CommentSection {}
 const CommentSection: FC<CommentSection> = ({}) => {
   //TODO:  use vaul ui package for sidebar
 
- return (
-    <div className="w-full">
-
-      <div className=" my-4 border-b border-gray-100 pb-4">
-              <div className="flex mb-2 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
-                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
-                  Dashboard
-                </h3>
-              </div>
-            </div>
-        
-
-        
+  return (
+    <div className="w-full h-screen">
+      <div className="h-[80%] text-xl m-4 shadow-sm shadow-gray-500 rounded-md border-gray-100 p-4">
+        <h1 className="text-2xl text-gray-600 text-center">Add Comments for Editors</h1>
+        <div>
+          
+        </div>
+      </div>
     </div>
   );
 };
