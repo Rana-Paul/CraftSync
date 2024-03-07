219007fbf73a41f832338e9b0009d7cb84cc1415
Rana Paul <ranapaul741223@gmail.com>
2024-03-07 19:23:20 +0530

    add responsive video with react-player

---
+++ src/components/Workspace/VideoPage/Video.tsx
@@ -0,0 +1,40 @@
+import { Loader2 } from 'lucide-react';
+import { FC, useEffect, useState } from 'react'
+import ReactPlayer from "react-player";
+
+
+interface VideoProps {
+  
+}
+
+const Video: FC<VideoProps> = ({}) => {
+    const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+    setIsClient(true)
+    }
+  }, []);
+    return (
+        <div suppressHydrationWarning={true} className="aspect-videon relative">
+    
+          {isClient ? (
+            <ReactPlayer
+            width="100%"
+            height="auto"
+            className="w-full h-full "
+            url="https://utfs.io/f/b99b4e90-1c38-4836-b69a-85c708d1dd7d-1uswaj.mp4"
+            controls={true}
+            // light is usefull incase of dark mode
+            light={false}
+            // picture in picture
+            pip={true}
+            style={{borderRadius: "100px",}}
+          />
+          ): <Loader2 className="h-4 w-4 animate-spin" />}
+        </div>
+        
+      );
+    
+}
+
+export default Video
--- src/components/Workspace/VideoPage/VideoPage.tsx
+++ src/components/Workspace/VideoPage/VideoPage.tsx
@@ -1,11 +1,17 @@
-import { FC } from 'react'
+import { Loader2 } from 'lucide-react';
+import { FC, useEffect, useState } from 'react'
+import ReactPlayer from "react-player";
+
 
 interface VideoPageProps {
   
 }
 
 const VideoPage: FC<VideoPageProps> = ({}) => {
-  return <div>VideoPage</div>
+
+  return (
+    <div></div>
+  )
 }
 
 export default VideoPage
