12c405c32f15999054a4860ddf5757b55474bf55
Rana Paul <ranapaul741223@gmail.com>
2024-03-07 19:26:11 +0530

    complete video design

--- src/components/Workspace/VideoPage/VideoPage.tsx
+++ src/components/Workspace/VideoPage/VideoPage.tsx
@@ -1,17 +1,22 @@
-import { Loader2 } from 'lucide-react';
-import { FC, useEffect, useState } from 'react'
+import { Loader2 } from "lucide-react";
+import { FC, useEffect, useState } from "react";
 import ReactPlayer from "react-player";
+import Video from "./Video";
 
-
-interface VideoPageProps {
-  
-}
+interface VideoPageProps {}
 
 const VideoPage: FC<VideoPageProps> = ({}) => {
-
   return (
-    <div></div>
-  )
-}
+    <div>
+      <div className="mx-auto max-w-6xl px-6 lg:px-8">
+        <div className="mt-8 flow-root sm:mt-5">
+          <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+            <Video />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default VideoPage
+export default VideoPage;
