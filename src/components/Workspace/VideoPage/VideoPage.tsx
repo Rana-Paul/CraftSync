ccc8803eaf778cc019d33bbc4d80411fe466e188
Rana Paul <ranapaul741223@gmail.com>
2024-03-08 18:40:00 +0530

    create basic form for video

--- src/components/Workspace/VideoPage/VideoPage.tsx
+++ src/components/Workspace/VideoPage/VideoPage.tsx
@@ -1,16 +1,28 @@
 import { Loader2 } from "lucide-react";
 import { FC } from "react";
 import Video from "./Video";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface VideoPageProps {}
 
 const VideoPage: FC<VideoPageProps> = ({}) => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    resetField,
+  } = useForm({
+    resolver: zodResolver(""),
+  });
   
   // Get metadata from db
 
   return (
-    <div>
-      <div className="mx-auto max-w-4xl px-6 lg:px-8">
+    <MaxWidthWrapper>
+      <div className="mx-auto max-w-6xl px-6 lg:px-8">
         <div className="mt-8 flow-root sm:mt-5">
           <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
             <Video url="https://utfs.io/f/b99b4e90-1c38-4836-b69a-85c708d1dd7d-1uswaj.mp4"/>
@@ -22,7 +34,7 @@ const VideoPage: FC<VideoPageProps> = ({}) => {
       <div>
         
       </div>
-    </div>
+    </MaxWidthWrapper>
   );
 };
 
