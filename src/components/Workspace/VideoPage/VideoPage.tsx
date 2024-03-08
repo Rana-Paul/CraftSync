8c9f61991ea8dc8dc458cc0c97a34a5b21ceaded
Rana Paul <ranapaul741223@gmail.com>
2024-03-08 18:32:20 +0530

    change video aspect retion

--- src/components/Workspace/VideoPage/VideoPage.tsx
+++ src/components/Workspace/VideoPage/VideoPage.tsx
@@ -1,6 +1,5 @@
 import { Loader2 } from "lucide-react";
-import { FC, useEffect, useState } from "react";
-import ReactPlayer from "react-player";
+import { FC } from "react";
 import Video from "./Video";
 
 interface VideoPageProps {}
@@ -11,7 +10,7 @@ const VideoPage: FC<VideoPageProps> = ({}) => {
 
   return (
     <div>
-      <div className="mx-auto max-w-6xl px-6 lg:px-8">
+      <div className="mx-auto max-w-4xl px-6 lg:px-8">
         <div className="mt-8 flow-root sm:mt-5">
           <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
             <Video url="https://utfs.io/f/b99b4e90-1c38-4836-b69a-85c708d1dd7d-1uswaj.mp4"/>
