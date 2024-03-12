1f1b6c9e0ca46ff15cdbd7e2002db34c61bfc09a
Rana Paul <ranapaul741223@gmail.com>
2024-03-12 20:05:58 +0530

    created generatedUploadUrl function

--- src/app/helpers/s3client.ts
+++ src/app/helpers/s3client.ts
@@ -1,4 +1,5 @@
-import {S3Client} from '@aws-sdk/client-s3'
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3'
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
 
 export const s3Client = new S3Client({
     region: 'us-east-1',
@@ -6,4 +7,14 @@ export const s3Client = new S3Client({
         accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
     },
-})
+});
+
+export const generateUploadUrl = (key:string, contentType: string) => {
+    const command = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME as string,
+        Key: key,
+        ContentType: contentType,
+    });
+
+    const url = getSignedUrl(s3Client, command, { expiresIn: 28800 });
+}
