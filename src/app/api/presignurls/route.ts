95503a80b0f5b5d791c94c2efd18bbe82a523d87
Rana Paul <ranapaul741223@gmail.com>
2024-03-16 19:51:14 +0530

    add route for getobject presign url

--- src/app/api/presignurls/route.ts
+++ src/app/api/presignurls/route.ts
@@ -2,7 +2,7 @@ import { db } from "@/db";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 import { NextRequest, NextResponse } from "next/server";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { s3Client } from "@/app/helpers/s3client";
 
@@ -22,6 +22,23 @@ export async function GET(request: NextRequest) {
     });
 
     const url = await getSignedUrl(s3Client, command, { expiresIn: 28800 });
+    console.log(url);
+    return NextResponse.json({ url });
+  } catch (error) {
+    return new Error("Something went wrong");
+  }
+}
+export async function POST(request: NextRequest) {
+  const { key } = await request.json();
+  // throw new Error();
+
+  try {
+    const command = new GetObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME as string,
+      Key: key as string,
+    });
+
+    const url = await getSignedUrl(s3Client, command, { expiresIn: 7 * 24 * 60 * 60 });
     console.log(url);
     return NextResponse.json({ url });
   } catch (error) {
