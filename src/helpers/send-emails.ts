fdfa8dd3112cab85cdde1ff4c98f7094fb9dc76f
Rana Paul <ranapaul741223@gmail.com>
2024-04-14 19:09:24 +0530

    create email function for review email

--- src/helpers/send-emails.ts
+++ src/helpers/send-emails.ts
@@ -1,6 +1,6 @@
 import { PrimaryActionEmailHtml } from '@/components/emails/InvitationEmail'
 import {Resend} from 'resend'
-import { InvitationEmailType } from '../lib/validators/invitationEmail';
+import { InvitationEmailType, ReviewContentEmailType } from '../lib/validators/invitationEmail';
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 export async function invitationEmail ({email, invitation_code, workspaceName, workspaceId}: InvitationEmailType) {
@@ -21,6 +21,26 @@ export async function invitationEmail ({email, invitation_code, workspaceName, w
     
     return data.data;
 }
+export async function reviewEmail ({email, workspaceName, editorName}: ReviewContentEmailType) {
+    console.log("review email");
+    
+    const data = await resend.emails.send({
+        from: 'CraftSync <fullstack@ranathedeveloper.site>',
+        to: [email],
+        subject:
+        `${editorName} has invited you to review the content of ${workspaceName} workspace`,
+        html: PrimaryActionEmailHtml({
+          actionLabel: "Review Content",
+          buttonText: "Review Content",
+          href: `${process.env.NEXT_PUBLIC_URL}/dashboard/${workspaceName}`,
+        }),
+    });
+    console.log(data.data);
+    
+    return data.data;
+}
+
+
 
 // TODO: create a function for sending review email
 // TODO: create a new email  template for review
