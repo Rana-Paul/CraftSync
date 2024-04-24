ff1943a6b7340f0a0d4d44ae4872ddec2a3432b9
Rana Paul <ranapaul741223@gmail.com>
2024-04-24 20:43:00 +0530

    update: api route protected

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,21 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   //   console.log("middleware");
   const token = await getToken({ req: request });
-  //   console.log("midd token" + token?.email);
+  //  console.log("midd token" + token?.email);
   //   console.log(request.nextUrl.pathname);
 
-  // TODO: Add routes 
-  // const authURLS = ["/dashboard", "/api/workspace", "dashboard/"];
+  // TODO: Add routes
+  const apiRoutes = [
+    "/api/editors",
+    "/api/getvideometadata",
+    "/api/review",
+    "/api/presignurls",
+    "/api/invitation",
+    "/api/mediaupdate",
+    "/api/videometadataupdate",
+    "/api/workspace",
+    "/api/dashboard",
+  ];
 
   if (token && request.nextUrl.pathname == "/api/auth/signin") {
     return NextResponse.redirect(new URL("/", request.url));
@@ -16,12 +26,15 @@ export async function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.includes("/dashboard") && !token) {
     return NextResponse.redirect(new URL("/api/auth/signin", request.url));
   }
+  if (apiRoutes.includes(request.nextUrl.pathname) && !token) {
+    return NextResponse.redirect(new URL("/api/auth/signin", request.url));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/api/auth/signin", "/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/api/:path*"],
 };
 
 // /api/auth/:path*
